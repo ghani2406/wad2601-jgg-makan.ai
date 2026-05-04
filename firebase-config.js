52e30cd71df5cb19c6bb3787a179dfa28b1c2dae
ghani2406 <ghanimuzakkimh@gmail.com>
2026-05-04 11:28:57 +0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDiUn1geiAd8FHNQtR6L6VcR-hzc_1Ux44",
-  authDomain: "wad2601jgg-b9561.firebaseapp.com",
-  projectId: "wad2601jgg-b9561",
-  storageBucket: "wad2601jgg-b9561.firebasestorage.app",
-  messagingSenderId: "1077286369453",
-  appId: "1:1077286369453:web:0bd30f4678a99090dbee24"
-};
